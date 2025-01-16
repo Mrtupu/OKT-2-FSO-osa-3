b8ae4367ed7518fb5c0db46e7124697967262900
Tupukda <teemu.s.harma@gmail.com>
2025-01-16 14:09:27 +0200

    10

--- index.js
+++ index.js
@@ -35,7 +35,9 @@ app.get('/api/persons', (req, res) => {
 
 app.get('/info', (req, res) => {
     const date = new Date()
-    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+    Person.find({}).then(persons => {
+        res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)    
+    })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
